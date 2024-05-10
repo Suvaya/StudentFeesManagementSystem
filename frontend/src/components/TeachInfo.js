d5a236c752b0f5ac938d2e2ffd580f4b6d6b19a4
Alish545 <alish55tuladhar@gmail.com>
2024-05-10 12:21:55 +0545

    teacher student done

--- frontend/src/components/TeachInfo.js
+++ frontend/src/components/TeachInfo.js
@@ -6,9 +6,11 @@ import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
-import sms from "../images/student.png"; 
-
-// const apiUrl = process.env.REACT_APP_API_URL;
+import sms from "../images/student.png";
+import "../App.css";
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DateCalendar } from '@mui/x-date-pickers/DateCalendar'; // Import your calendar component
 
 const TeachInfo = () => {
     const { userId } = useAuth();
@@ -66,46 +68,59 @@ const TeachInfo = () => {
             <div style={{ display: 'flex' }}>
                 <div className="widget">
                     <div className="left">
-                        <span className="title">Total Revenue</span>
+                        <span className="title">Salary</span>
                         <span className="counter">${teacherDetails.salary.toLocaleString()}</span>
+                        <span className="link"></span>
                     </div>
                 </div>
                 <div className="widget1">
                     <div className="left">
                         <span className="title">Number of Subject</span>
                         <span className="counter1">{teacherDetails.subjectsTaught.length}</span>
+                        <span className="link"></span>
                     </div>
                 </div>
                 <div className="widget1">
                     <div className="left">
                         <span className="title">Notice</span>
-                        <span className="counter1"></span>
+                        <span className="counter1">{}</span>
+                        <span className="link"><i>Important News will be display here</i></span>
                     </div>
                 </div>
             </div>
-            <h2 align="center" className='Teachtable'>Teacher Details</h2>
-             <TableContainer className='Teachtable'>
-                <Table aria-label="simple table">
-                  <TableHead>
-                    <TableRow>
-                      <TableCell className="tables" align="center"><strong>Full name</strong></TableCell>
-                      <TableCell className="tables" align="center"><strong>Email</strong></TableCell>
-                      <TableCell className="tables" align="center"><strong>Address</strong></TableCell>
-                      <TableCell className="tables" align="center"><strong>Phone Number</strong></TableCell>
-                      <TableCell className="tables" align="center"><strong>Subjects Taught</strong></TableCell>
-                      <TableCell className="tables" align="center"><strong>Salary</strong></TableCell>
-                    </TableRow>
-                  </TableHead>
-                  <TableBody>
-                        <TableCell align="center">{teacherDetails.fullName}</TableCell>
-                        <TableCell align="center">{teacherDetails.email}</TableCell>
-                        <TableCell align="center">{teacherDetails.address}</TableCell>
-                        <TableCell align="center">{teacherDetails.phoneNumber}</TableCell>
-                        <TableCell align="center">{teacherDetails.subjectsTaught.join(", ")}</TableCell> {/* Assuming subjectsTaught is an array */}
-                        <TableCell align="center">${teacherDetails.salary.toLocaleString()}</TableCell> {/*Format salary as a readable string*/}
-                  </TableBody>
-                </Table>
-             </TableContainer>
+            <div style={{ display: 'flex' }}>
+                <div>
+                    <h2 align="center" className='Teachtable'>Teacher Details</h2>
+                     <TableContainer className='Teachertable'>
+                        <Table aria-label="simple table">
+                          <TableHead>
+                            <TableRow>
+                              <TableCell className="tables" align="center"><strong>Full name</strong></TableCell>
+                              <TableCell className="tables" align="center"><strong>Email</strong></TableCell>
+                              <TableCell className="tables" align="center"><strong>Address</strong></TableCell>
+                              <TableCell className="tables" align="center"><strong>Phone Number</strong></TableCell>
+                              <TableCell className="tables" align="center"><strong>Subjects Taught</strong></TableCell>
+                              <TableCell className="tables" align="center"><strong>Salary</strong></TableCell>
+                            </TableRow>
+                          </TableHead>
+                          <TableBody>
+                                <TableCell align="center">{teacherDetails.fullName}</TableCell>
+                                <TableCell align="center">{teacherDetails.email}</TableCell>
+                                <TableCell align="center">{teacherDetails.address}</TableCell>
+                                <TableCell align="center">{teacherDetails.phoneNumber}</TableCell>
+                                <TableCell align="center">{teacherDetails.subjectsTaught.join(", ")}</TableCell> {/* Assuming subjectsTaught is an array */}
+                                <TableCell align="center">${teacherDetails.salary.toLocaleString()}</TableCell> {/*Format salary as a readable string*/}
+                          </TableBody>
+                        </Table>
+                     </TableContainer>
+                </div>
+                {/* Calendar */}
+                <div style={{ marginLeft: '250px', marginRight: '100px' }}>
+                    <LocalizationProvider dateAdapter={AdapterDayjs}>
+                        <DateCalendar />
+                    </LocalizationProvider>
+                </div>
+            </div>
         </div>
     );
 };
